9fbf30fac8aedb45fdf5b7d72b5682233ff5cfff
William Wu <william@pylabs.org>
2018-09-08 14:26:14 +0800

    完成前台 bs4 樣板架構與 login 畫面

--- tp_contest/views.py
+++ tp_contest/views.py
@@ -3,4 +3,8 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/home.jinja2')
 def home_view(request):
-    return {'project': 'tp_contest'}
+    return {}
+
+@view_config(route_name='login', renderer='templates/login.jinja2')
+def login_view(request):
+    return {}
