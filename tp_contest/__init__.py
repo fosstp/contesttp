8c96e8dc7bd5932eac7b7bc4aabe271a25f60a85
William Wu <william@pylabs.org>
2018-09-08 14:51:37 +0800

    新增活動列表頁面與調整統一前台風格

--- tp_contest/__init__.py
+++ tp_contest/__init__.py
@@ -19,5 +19,6 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('login', '/login')
+    config.add_route('list_activities', '/activities')
     config.scan()
     return config.make_wsgi_app()
--- tp_contest/views.py
+++ tp_contest/views.py
@@ -5,6 +5,12 @@ from pyramid.view import view_config
 def home_view(request):
     return {}
 
+
 @view_config(route_name='login', renderer='templates/login.jinja2')
 def login_view(request):
+    return {}
+
+
+@view_config(route_name='list_activities', renderer='templates/list_activities.jinja2')
+def list_activities_view(request):
     return {}
